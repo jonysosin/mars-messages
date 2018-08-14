1a5fe900bd8f1c08f34c784e8bc3dfb965c42a27
Jonathan Sosin <jonysosin@gmail.com>
2018-08-14 00:22:09 -0300

    A

--- routes.js
+++ routes.js
@@ -7,4 +7,8 @@ router.get('/status', (ctx) => {
   ctx.body = { status: 'online' }
 })
 
+router.all('/webhook', async(ctx) => {
+    console.log(JSON.stringify({ get: ctx.query, post: ctx.request.body }, null, 2))
+})
+
 module.exports = router
