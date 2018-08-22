e126a079920b2e3468c378e9369c8d42e39000f7
Jonathan Sosin <jonysosin@gmail.com>
2018-08-22 16:46:51 -0300

    Datos

--- controllers/message.js
+++ controllers/message.js
@@ -1,5 +1,4 @@
 const APP_ID = '5b7c61b228909c00220958f5'
-const USER_ID = 'ed85fd7224dd6a6856cf0fc1'
 const KEY_ID = 'app_5b7d97b20260730022c2116b'
 const SECRET = 'jdKm9NzyC1v_GSqh60sV1NZysavrVVugRgYvqmYYdqCQ_006RwTe0IFwt_6Io3ujX35X5jpbjy1tXHoXVTANHQ'
 const SmoochCore = require('smooch-core')
@@ -13,23 +12,11 @@ module.exports = class Message {
     })
   }
 
-  async sendMessage (userId) {
+  async sendMessage (userId, message) {
     const response = await this.smooch.appUsers.sendMessage({
       appId: APP_ID,
       userId: userId,
-      message: {
-        role: 'appMaker',
-        type: 'image',
-        text: 'Hello!',
-        mediaUrl: 'https://uploads-ssl.webflow.com/589c81e8313ec1b057657bc4/5a09b287e1339f0001a80a2b_octopuspng-p-1080.png'
-        // actions: [
-        //   {
-        //     text: 'Hola! Soy un pulpo',
-        //     type: 'link',
-        //     uri: 'https://uploads-ssl.webflow.com'
-        //   }
-        // ]
-      }
+      message
     })
 
     console.log(`Response of send: ${response}`)
@@ -38,6 +25,19 @@ module.exports = class Message {
   async onReceiveMessage (data) {
     const userId = data.appUser._id
     console.log(`I received a message of: ${userId}`)
+    this.sendMessage(userId, {
+      role: 'appMaker',
+      type: 'image',
+      text: 'Hello!',
+      mediaUrl: 'https://uploads-ssl.webflow.com/589c81e8313ec1b057657bc4/5a09b287e1339f0001a80a2b_octopuspng-p-1080.png',
+      actions: [
+        {
+          text: 'Sirena!',
+          type: 'link',
+          uri: 'getsirena.com'
+        }
+      ]
+    })
   }
 
   async on (data) {
